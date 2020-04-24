c10c107ee3f3b501d0c4fda07ae7a8e11205ba39
nutpattara <itsnutp@gmail.com>
2020-04-24 10:08:31 +0700

    scrap flask for django lmao

---
+++ covidchat/chatroom_user/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChatroomUserConfig(AppConfig):
+    name = 'chatroom_user'
---
+++ covidchat/chatroom_user/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.contrib.auth import authenticate, login, logout
+
+def index(request):
+    return render(request, 'chatroom_user/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        register_data = request.POST.dict()
+        username = register_data.get('username')
+        password = register_data.get('password')
+        if not User.objects.filter(username=username).exists():
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+            login_user = authenticate(username = username, password = password)
+            login(request, login_user)
+            return redirect('index')
+
+        return HttpResponse('registration error')
+
+    return render(request, 'registration/register.html')
